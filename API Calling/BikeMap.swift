586ea013eaec4525d61c4d9c2232f4185fde45a1
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-02-28 11:08:00 -0600

    Parsed the Json data and fixed the problem with the load error method

--- API Calling/BikeMap.swift
+++ API Calling/BikeMap.swift
@@ -17,11 +17,27 @@ class BikeViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "Networks"
         let query = "https://api.citybik.es/v2/networks"
-    }
+        if let url = URL(string: query){
+            if let data = try?  Data(contentsOf:url){
+                let json = try! JSON(data:data)
+                if json["status"] == "ok"{
+                    parse(json:json)
+                    return
+                }
+                
+            }
+        }
+        else{
+             loadError()
+        }
+        
+      }
+    
+
     
     func parse(json:JSON){
         for result in json["networks"].arrayValue{
-            let company = result["company"].stringValue
+            let company = result["c"].stringValue
             let id = result["id"].stringValue
             let location = result["location"].stringValue
             let network = ["company":company,"id":id,"location":location]
@@ -35,6 +51,18 @@ class BikeViewController: UITableViewController {
         alert.addAction(UIAlertAction(title:"Ok", style: .default, handler:nil))
         present(alert,animated:true, completion:nil)
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return networks.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"Cell", for:indexPath)
+        let network = networks[indexPath.row]
+        cell.textLabel?.text = network["company"]
+        cell.detailTextLabel?.text = network["id"]
+        return cell
+    }
+    
     
 }
 
